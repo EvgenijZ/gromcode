ab5c08fdeee6f674e21b17ecc7b4fc924309eeb9
EvgenijZ <leetruk12@gmail.com>
2022-02-17 01:04:36 +0200

    Upload js task 7

---
+++ lesson-selectors/jstask7/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const tasks = [
+    { text: 'Buy milk', done: false },
+    { text: 'Pick up Tom from airport', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
+    { text: 'Buy meat', done: true },
+];
+
+/**
+ * @param {object[]} tasksList
+ * @return {undefined}
+ */
+const renderTasks = tasksList => {
+    const list = document.querySelector('.list');
+
+    const items = tasksList.map(({ text, done }) => {
+        console.log(text);
+        const li = document.createElement('li');
+        li.classList.add('list__item');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = done;
+        checkbox.classList.add('list__item-checkbox');
+
+        li.append(checkbox, text);
+
+        return li;
+    });
+
+    list.append(...items);
+};
+
+renderTasks(tasks);
