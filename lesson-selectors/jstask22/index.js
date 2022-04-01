b02eb8de26ed3cdc98ac1cd5381d1254fbfbd814
EvgenijZ <leetruk12@gmail.com>
2022-04-01 13:07:00 +0300

    Upload js task 22

---
+++ lesson-selectors/jstask22/index.js
@@ -0,0 +1,15 @@
+import { addImageV2 } from './addImageV.js';
+
+const addImageV2 = url => {
+    return new Promise((resolve, reject) => {
+        addImage(url, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+};
+
+addImageV2('https://s3.envato.com/files/380251776/Preview_Screenshots/01_imagePdfCreator.png')
+    .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+    .catch(error => console.log(error)); // ==> 'Image load failed'
+
