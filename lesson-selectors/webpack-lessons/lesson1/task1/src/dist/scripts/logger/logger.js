c6111dae1c58b4981beb1386ffc0c2c8db6f9cde
Eugenij <leetruk12@gmail.com>
2022-04-03 19:25:51 +0300

    Delete lesson-selectors/webpack-lessons/lesson1/task1/src/dist directory

--- lesson-selectors/webpack-lessons/lesson1/task1/src/dist/scripts/logger/logger.js
+++
@@ -1,9 +0,0 @@
-export const createLogger = (loggerName) => {
-    const logs = [];
-
-    return {
-        getLogs: () => logs,
-        log: message => logs.push(`log - ${loggerName} - ${message}`),
-        error: error => logs.push(`error - ${loggerName} - ${error}`),
-    }
-}
