4531599b56f0974953aa00dfdd6bfa1bec783bdb
Eugenij <leetruk12@gmail.com>
2022-04-05 19:25:31 +0300

    Delete lesson-selectors/webpack1 directory

--- lesson-selectors/webpack1/src/scripts/logger/logger.js
+++
@@ -1,9 +0,0 @@
-export const createLogger = (name) => {
-    const logs = [];
-
-    return {
-        log: (message) => logs.push(`log - ${name} - ${message}`),
-        error: (message) => logs.push(`error - ${name} - ${message}`),
-        getLogs: () => logs
-    }
-}
