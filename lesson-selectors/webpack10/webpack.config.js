30f908b4b3707c772eeb4c23d0172379c757a3c4
EvgenijZ <leetruk12@gmail.com>
2022-04-07 19:55:58 +0300

    Upload webpack task 10

---
+++ lesson-selectors/webpack10/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        dashboard: './src/dashboard/index.js',
+        profile: './src/profile/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'build')
+    }
+}
