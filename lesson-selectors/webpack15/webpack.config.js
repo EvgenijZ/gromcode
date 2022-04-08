0b0cd8d66ea72a4a3b9451fbe00bfbad2319fdf9
EvgenijZ <leetruk12@gmail.com>
2022-04-08 13:00:45 +0300

    Upload webpack task 15

---
+++ lesson-selectors/webpack15/webpack.config.js
@@ -0,0 +1,37 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name].[ext]',
+                            outputPath: 'images'
+                        },
+                    },
+                ],
+            }
+        ],
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin(),
+            new MiniCssExtractPlugin()
+        ]
+    }
+}
