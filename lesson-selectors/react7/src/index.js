e65cf663c53f7313257dff047d63f2fcdfc84b7b
EvgenijZ <leetruk12@gmail.com>
2022-04-09 14:32:35 +0300

    Upload react task 7

---
+++ lesson-selectors/react7/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootEl = document.querySelector('#root');
+
+const spanStyles = {
+    color: 'red', fontWeight: 700
+}
+
+const loginForm = (
+    <main className="page">
+        <form className="login-form">
+            <h1 className="form-title">Profile</h1>
+            <div className="form-control">
+                <label className="form-label" htmlFor="name">Name</label>
+                <input className="form-input" type="text" id="name" name="name" value="anonymous" />
+            </div>
+            <div className="form-control">
+                <label className="form-label" id="age" htmlFor="age">Age</label>
+                <input className="form-input" type="number" value="17" name="age" />
+                <span style={spanStyles}>To young</span>
+            </div>
+            <button className="submit-button" type="submit">Submit</button>
+        </form>
+    </main>
+);
+
+ReactDOM.render(loginForm, rootEl);
