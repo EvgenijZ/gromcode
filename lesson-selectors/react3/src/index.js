fc654f1994925ef8520a5126088d2414fcd54a34
EvgenijZ <leetruk12@gmail.com>
2022-04-09 13:01:25 +0300

    Upload react tasks

---
+++ lesson-selectors/react3/src/index.js
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+const container = document.querySelector('#root');
+
+const renderGreeting = elem => {
+  const root = createRoot(elem);
+
+  root.render(
+    <div className="greeting">
+      <div className="greeting__title">Hello, world!</div>
+      <div className="greeting__text">I'm learning React</div>
+    </div>
+  );
+
+}
+
+renderGreeting(container);
