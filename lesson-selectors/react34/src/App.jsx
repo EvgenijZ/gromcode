99a22cbdeaa4fcc14b483d61f063fcdfeecfb3a2
EvgenijZ <leetruk12@gmail.com>
2022-04-11 18:22:19 +0300

    Upload react task 34

---
+++ lesson-selectors/react34/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dimensions from './Dimensions.jsx';
+
+class App extends React.Component {
+	render() {
+		return <Dimensions />;
+	}
+}
+
+export default App;
---
+++ lesson-selectors/react34/src/Dimensions.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class Dimensions extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			width: null,
+			height: null,
+		};
+
+		this.setDimensions = this.setDimensions.bind(this);
+	}
+
+	componentDidMount() {
+		window.addEventListener('resize', this.setDimensions);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.setDimensions);
+	}
+
+	setDimensions(e) {
+		this.setState({ width: e.target.innerWidth, height: e.target.innerHeight });
+		document.title = `${this.state.width} x ${this.state.height}`;
+	}
+
+	render() {
+		return (
+			this.state.width && (
+				<div>{`${this.state.width} x ${this.state.height}`}</div>
+			)
+		);
+	}
+}
