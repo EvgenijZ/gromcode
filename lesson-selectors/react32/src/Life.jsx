79a73c7e284d647107b7c37e53abd4d88d86a883
EvgenijZ <leetruk12@gmail.com>
2022-04-11 15:57:14 +0300

    Upload react task 32

---
+++ lesson-selectors/react32/src/Life.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default class Life extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			updateCounter: 0,
+		};
+
+		console.log('constructor: good place to create state');
+	}
+
+	componentDidMount() {
+		console.log('componentDidMount: API calls, subscriptions');
+		this.updateCounter();
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		if (nextState.updateCounter < 2) return false;
+		console.log(
+			'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
+		);
+		return true;
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.updateCounter > 1)
+			console.log(
+				'componentDidUpdate(prevProps, prevState): some updates based on new props'
+			);
+	}
+
+	componentWillUnmount() {
+		console.log(
+			'componentWillUnmount(): cleanup before DOM related to component will be removed'
+		);
+	}
+
+	updateCounter() {
+		setInterval(
+			() => this.setState({ updateCounter: this.state.updateCounter + 1 }),
+			2000
+		);
+	}
+
+	render() {
+		console.log('return React element to build DOM');
+		return this.state.updateCounter < 3 && <div>123</div>;
+	}
+}
