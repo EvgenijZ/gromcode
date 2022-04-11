b57fde46af3d7ae7551db4e18b5ff245fbf3c5c8
EvgenijZ <leetruk12@gmail.com>
2022-04-11 19:34:21 +0300

    Upload react task 36

---
+++ lesson-selectors/react36/src/ConnectionStatus.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class ConnectionStatus extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isOnline: true,
+		};
+	}
+
+	componentDidMount() {
+		window.addEventListener('online', () => this.onOnline(true));
+		window.addEventListener('offline', () => this.onOnline(false));
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener(['online', 'offline'], () => this.onOnline);
+	}
+
+	onOnline(status) {
+		this.setState({ isOnline: status });
+	}
+
+	render() {
+		return this.state.isOnline ? (
+			<div className='status'>Online</div>
+		) : (
+			<div className='status status_offline'>Offline</div>
+		);
+	}
+}
