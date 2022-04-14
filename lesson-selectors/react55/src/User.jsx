7fe4da62bcce58614c08e3541cbe6ecbb5461b95
EvgenijZ <leetruk12@gmail.com>
2022-04-14 18:43:08 +0300

    Upload react task 55

---
+++ lesson-selectors/react55/src/User.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const User = () => {
+	const { userId } = useParams();
+	const [user, setUser] = useState({});
+
+	const fetchUser = async (id) => {
+		try {
+			const response = await fetch(`https://api.github.com/users/${id}`);
+			if (!response.ok) throw new Error(`Fetching ${id} failed`);
+			return await response.json();
+		} catch (error) {
+			alert(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchUser(userId)
+			.then((user) => setUser(user))
+			.catch((e) => console.error(e));
+	}, [userId]);
+
+	return (
+		<div className='user'>
+			<img alt='User Avatar' src={user.avatar_url} className='user__avatar' />
+			<div className='user__info'>
+				<span className='user__name'>{user.name}</span>
+				<span className='user__location'>{user.location}</span>
+			</div>
+		</div>
+	);
+};
+
+export default User;
