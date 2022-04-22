1378cb036c1ccad55696788d8b54f0b3c6920c80
EvgenijZ <leetruk12@gmail.com>
2022-04-22 03:52:54 +0300

    Upload redux task 2

---
+++ lesson-selectors/redux_task2/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Header from './Header.jsx';
+import { UserContext } from './user.js';
+
+const App = () => {
+	const [user, setUser] = useState({
+		name: 'Nikola Tesla',
+		avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+	});
+	return (
+		<UserContext.Provider value={user}>
+			<div className='page'>
+				<Header />
+			</div>
+		</UserContext.Provider>
+	);
+};
+
+export default App;
