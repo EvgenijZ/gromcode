f142d0e724b8c2afe021682334b8736f3661b9f6
EvgenijZ <leetruk12@gmail.com>
2022-04-22 23:46:07 +0300

    Update store

--- lesson-selectors/redux_task8/src/language.reducer.js
+++ lesson-selectors/redux_task8/src/language.reducer.js
@@ -1,14 +1,11 @@
 import { SET_LANGUAGE } from "./language.actions";
 
-const initialState = { language: 'en' };
+const initialState = 'en';
 
 export const languageReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_LANGUAGE:
-            return {
-                ...state,
-                language: action.payload
-            }
+            return action.payload;
         default:
             return state;
     }
--- lesson-selectors/redux_task8/src/user.reducer.js
+++ lesson-selectors/redux_task8/src/user.reducer.js
@@ -1,19 +1,14 @@
 import { SET_USER, REMOVE_USER } from "./user.actions";
 
-const initialState = { user: null };
-
-export const userReducer = (state = initialState, action) => {
+export const userReducer = (state = null, action) => {
     switch (action.type) {
         case SET_USER:
             return {
                 ...state,
-                user: { ...action.payload }
+                ...action.payload
             }
         case REMOVE_USER:
-            return {
-                ...state,
-                user: null
-            }
+            return null;
         default: return state;
     }
 }
