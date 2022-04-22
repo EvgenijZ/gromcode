1d9988cd28b78df0a4c5b4ec03fc9a7d55c4071e
EvgenijZ <leetruk12@gmail.com>
2022-04-22 20:15:23 +0300

    Update counter state

--- lesson-selectors/redux_task6/src/counter.reducer.js
+++ lesson-selectors/redux_task6/src/counter.reducer.js
@@ -1,15 +1,15 @@
 import { INCREMENT, DECREMENT, RESET } from './counter.actions'
 
-const initialState = { counter: 0 };
+const initialState = 0;
 
 export default function userReducer(state = initialState, action) {
     switch (action.type) {
         case INCREMENT:
-            return { counter: state.counter + 1 };
+            return state + 1;
         case DECREMENT:
-            return { counter: state.counter - 1 };
+            return state - 1;
         case RESET:
-            return { counter: state.counter = 0 };
+            return 0;
         default:
             return state;
     }
