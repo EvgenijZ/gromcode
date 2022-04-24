1f23cc5d28af4fec7d388b1304a25279250ef94a
EvgenijZ <leetruk12@gmail.com>
2022-04-24 22:19:42 +0300

    Update Filter component

--- lesson-selectors/redux_task14/src/Filter.jsx
+++ lesson-selectors/redux_task14/src/Filter.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export default class Filter extends React.Component {
-	render() {
-		return (
-			<div className='filter'>
-				<span className='filter__count'>{this.props.count}</span>
-				<input
-					type='text'
-					className='filter__input'
-					value={this.props.filterText}
-					onChange={(e) => this.props.onChange(e.target.value)}
-				/>
-			</div>
-		);
-	}
-}
+const Filter = ({ count, filterText, onChange }) => {
+	return (
+		<div className='filter'>
+			<span className='filter__count'>{count}</span>
+			<input
+				type='text'
+				className='filter__input'
+				value={filterText}
+				onChange={(e) => onChange(e.target.value)}
+			/>
+		</div>
+	);
+};
+
+export default Filter;
